72802cf8c41c8a01191e3e543dd8b12187438a2c
HisMonDon <revenge.ez.123@gmail.com>
2024-10-07 10:51:32 -0400

    Update Lesson1.py

--- Unit 4/Lesson1.py
+++ Unit 4/Lesson1.py
@@ -1,1 +1,9 @@
+print("Type in a message, and I'll display it five times.")
 
+message = input("Message: ")
+times = int(input("How many times? "))
+print()
+n = 1
+while n <= times:
+    print(f"{n*10}. {message}")
+    n += 1
