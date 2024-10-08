adca4c6b48e374eb169768e2b1bed3475a95073a
HisMonDon <revenge.ez.123@gmail.com>
2024-10-08 10:20:11 -0400

    Update adding_variables_in_loop.py

--- Unit 4/Lesson 3/adding_variables_in_loop.py
+++ Unit 4/Lesson 3/adding_variables_in_loop.py
@@ -1,1 +1,12 @@
+print("I will add up the numbers you give me.")
 
+total = 0
+number = None
+
+while number != 0:
+    number = int(input("Number: "))
+    total += number
+    if number != 0:
+        print(f"The total so far is {total}")
+
+print(f"The total is {total}")
