e7a696afe14506b30c08b2e083936d08a25feb8b
HisMonDon <revenge.ez.123@gmail.com>
2024-10-08 10:13:39 -0400

    Update dice_doubles.py

--- Unit 4/Lesson 3/dice_doubles.py
+++ Unit 4/Lesson 3/dice_doubles.py
@@ -1,1 +1,14 @@
+import random
 
+print("HERE COMES THE DICE!")
+
+roll1 = roll2 = 0
+
+while roll1 != roll2:
+    roll1 = random.randint(1, 6)
+    roll2 = random.randint(1, 6)
+
+    print(f"Roll #1: {roll1}")
+    print(f"Roll #2: {roll2}")
+    total = roll1 + roll2
+    print(f"The total is {total}!\n")
