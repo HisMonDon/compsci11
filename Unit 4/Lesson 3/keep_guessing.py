8240848e05039b404f2bc6f802a1b4b8879fabee
HisMonDon <revenge.ez.123@gmail.com>
2024-10-08 10:12:09 -0400

    Update keep_guessing.py

--- Unit 4/Lesson 3/keep_guessing.py
+++ Unit 4/Lesson 3/keep_guessing.py
@@ -1,1 +1,18 @@
+import random
 
+# The number to guess
+number_to_guess = random.randint(1, 10)
+guess = None
+
+print("I have chosen a number between 1 and 10. Try to guess it.")
+
+# Keep guessing until the correct number is guessed
+while guess != number_to_guess:
+    guess = int(input("Your guess: "))
+
+    if guess < number_to_guess:
+        print("That is incorrect. Guess again.")
+    elif guess > number_to_guess:
+        print("That is incorrect. Guess again.")
+    else:
+        print("That's right! You're a good guesser.")
