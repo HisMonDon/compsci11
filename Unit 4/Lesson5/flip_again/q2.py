13c17d9f1b1ccfa6e923137428d4d08cf38a692a
HisMonDon <revenge.ez.123@gmail.com>
2024-10-09 10:21:07 -0400

    Create q1.py

---
+++ Unit 4/Lesson5/flip_again/q2.py
@@ -0,0 +1,17 @@
+import random
+
+
+again = 'y'
+
+while again == "y":
+    flip = random.randrange(2)  # 0-1
+
+    if flip == 1:
+        coin = "HEADS"
+    else:
+        coin = "TAILS"
+
+    print("You flip a coin and it is... " + coin)
+
+    again = input("Would you like to flip again (y/n)? ")
+#Answers on attached doc.
