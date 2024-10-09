29c2ed70f953a6fab9014aac1e682bc8919f21a4
HisMonDon <revenge.ez.123@gmail.com>
2024-10-09 10:38:51 -0400

    Update safe_square_root.py

--- Unit 4/Lesson5/safe_square_root.py
+++ Unit 4/Lesson5/safe_square_root.py
@@ -1,12 +1,12 @@
 import math
 
 print("SQUARE ROOT!")
+
 while True:
     number = float(input("Enter a number: "))
-    if number < 0:
-        print("You can't take the square root of a negative number, silly.")
-        continue
-    else:
-        sqrt_number = math.sqrt(number)
-        print(f"The square root of {number} is {sqrt_number}.")
+    if number >= 0:
         break
+    print("You can't take the square root of a negative number, silly.")        
+
+sqrt_number = math.sqrt(number)
+print(f"The square root of {number} is {sqrt_number}.")
