b4b0af1f2cce8b8549f03c563ae82557922c3e27
Eric Lu <revenge.ez.123@gmail.com>
2024-10-10 10:15:55 -0400

    Update counting_with_for_loop.py

--- Unit 4/Lesson 6/counting_with_for_loop.py
+++ Unit 4/Lesson 6/counting_with_for_loop.py
@@ -1,6 +1,21 @@
+#See if you can change the for loop so that the message starts at 2 and counts by twos, like so:
+#Type in a message, and I'll display it ten times.
+#Message: qwerty
+#2. qwerty
+#4. qwerty
+#6. qwerty
+#8. qwerty
+#10. qwerty
+#12. qwerty
+#14. qwerty
+#16. qwerty
+#18. qwerty
+#20. qwerty
+
+
 print("Type in a message, and I'll display it five times.")
 
 message = input("Message: ")
 
-for n in range(2, 20, 2):
+for n in range(2, 21, 2):
     print(f"{n}. {message}")
