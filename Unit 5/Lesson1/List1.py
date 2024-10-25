2b144c444e0314191a955cb003c4fe95f5670876
Eric Lu <revenge.ez.123@gmail.com>
2024-10-25 10:11:22 -0400

    Update List1.py

--- Unit 5/Lesson1/List1.py
+++ Unit 5/Lesson1/List1.py
@@ -1,1 +1,26 @@
 
+my_list = []
+
+# No for loop used
+my_list.append(-113)
+my_list.append(-113)
+my_list.append(-113)
+my_list.append(-113)
+my_list.append(-113)
+my_list.append(-113)
+my_list.append(-113)
+my_list.append(-113)
+my_list.append(-113)
+my_list.append(-113)
+
+# One element at a time
+print(f"Slot 0 contains a {my_list[0]}")
+print(f"Slot 1 contains a {my_list[1]}")
+print(f"Slot 2 contains a {my_list[2]}")
+print(f"Slot 3 contains a {my_list[3]}")
+print(f"Slot 4 contains a {my_list[4]}")
+print(f"Slot 5 contains a {my_list[5]}")
+print(f"Slot 6 contains a {my_list[6]}")
+print(f"Slot 7 contains a {my_list[7]}")
+print(f"Slot 8 contains a {my_list[8]}")
+print(f"Slot 9 contains a {my_list[9]}")
