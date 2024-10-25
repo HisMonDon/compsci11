efed897e13d758ba1e53586ca124ce88093a56a8
Eric Lu <revenge.ez.123@gmail.com>
2024-10-25 10:14:44 -0400

    Update List1.py

--- Unit 5/Lesson1/List1.py
+++ Unit 5/Lesson1/List1.py
@@ -1,1 +1,16 @@
+my_list = []
 
+# No for loop used
+my_list.append(-113)
+my_list.append(-113)
+my_list.append(-113)
+my_list.append(-113)
+my_list.append(-113)
+my_list.append(-113)
+my_list.append(-113)
+my_list.append(-113)
+my_list.append(-113)
+my_list.append(-113)
+
+for x in range(len(my_list)):
+  print(f"Slot {x} contains a {my_list[x]}.")
