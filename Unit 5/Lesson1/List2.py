c3f529a33b9e3feba9fc51ca32b86d4e51ece0bf
Eric Lu <revenge.ez.123@gmail.com>
2024-10-25 10:18:23 -0400

    Update List2.py

--- Unit 5/Lesson1/List2.py
+++ Unit 5/Lesson1/List2.py
@@ -1,1 +1,17 @@
+import random
 
+
+mylist = []
+mylist.append(random.randint(1, 100))
+mylist.append(random.randint(1, 100))
+mylist.append(random.randint(1, 100))
+mylist.append(random.randint(1, 100))
+mylist.append(random.randint(1, 100))
+mylist.append(random.randint(1, 100))
+mylist.append(random.randint(1, 100))
+mylist.append(random.randint(1, 100))
+mylist.append(random.randint(1, 100))
+mylist.append(random.randint(1, 100))
+
+for x in range(len(mylist)):
+    print(f"Slot {x} contains a {mylist[x]}")
