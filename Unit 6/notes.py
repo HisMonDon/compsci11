b280035b378035465ec4c5d70c98baba37f3cc21
Eric Lu <revenge.ez.123@gmail.com>
2024-11-19 10:16:51 -0500

    Update notes.py

--- Unit 6/notes.py
+++ Unit 6/notes.py
@@ -36,3 +36,33 @@ def multiply(a: int, b: int) -> int:
 #need to save in a variable
 result = multiply(5, 10)
 print(result)
+#-------------------------------------------------------------
+#Create a function that takes a number, adds 5 to the number, and returns the result. Store this in a variable and print it out.
+#Create another function that takes a name, creates a string greeting with the name int it, stores it in a variable, and prints it out
+#My solution:
+def addfive(number : int) -> int:
+    return number+5
+mynumber = addfive(3)
+print(mynumber)
+
+
+def greeter(name : str) -> str:
+    return f'Hello {name}!'
+greetstring = greeter("Eric")
+print(greetstring)
+
+#FUNCTIONS SHOULD NOT HAVE INPUT OR PRINT (except for main)
+#Main function:
+def addfive(number):
+    return number+5
+mynumber = addfive(3)
+print(mynumber)
+
+
+def greeter(name : str) -> str:
+    return f'Hello {name}!'
+def main():
+    greetstring = greeter("Eric")
+    print(greetstring)
+if __name__ == "__main__":
+    main()
